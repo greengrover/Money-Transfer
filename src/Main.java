5d01a81e84a874419514c32520aec9adeefadac4
Nikonov Vladzimir <nikonovbox@gmail.com>
2021-08-16 15:20:51 +0300

    bank transfer software

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance;
+        balance = 2_000_000_000;
+        int transfer;
+        transfer = 500_000_000;
+        int totalbalance;
+        totalbalance = balance + transfer;
+        System.out.println(totalbalance);
+    }
+}
